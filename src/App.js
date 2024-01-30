29165f7db1c33112d578383e397eb94a504ff0b6
Lwarentina <raditya.hanarta@gmail.com>
2024-01-30 13:58:21 +0700

    seg

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Utama from './Components/utama';
+import Navbar from './Components/navb'
 import {Link} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends React.Component {
   render() {
     return(
-      <div> <hr />
-        <Link to="/">Beranda</Link> |
-        <Link to="/aboutme">about me</Link> |
-        <Link to="/karya">Karya</Link> |
-        <Link to="/kontak">Kontak</Link> <hr />
+      <div><hr />
+        <button type="button" class="btn btn-outline-primary"><Link to="/">Beranda</Link> </button>|
+        <button type="button" class="btn btn-outline-primary"><Link to="/aboutme">Me</Link></button> |
+        <button type="button" class="btn btn-outline-primary"><Link to="/karya">Karya</Link></button> |
+        <button type="button" class="btn btn-outline-primary"><Link to="/kontak">Kontak</Link></button>
+        <hr />
         <p><Utama /></p>
       </div>
     );
